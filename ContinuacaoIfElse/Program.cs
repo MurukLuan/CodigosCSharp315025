dd3d18731c6d24a141bc72be679e654cf3b770d6
MurukLuan <murukluan@gmail.com>
2022-08-17 20:15:09 -0300

    Continuação da aula sobre if/else

---
+++ ContinuacaoIfElse/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+/*
+double teste = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine(teste.ToString("F2", CultureInfo.InvariantCulture));
+*/
+/*
+Console.WriteLine("Entre com o primeiro numero: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Entre com o segundo numero: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Min(num1, num2));
+
+Console.WriteLine(Math.Max(num1 , num2));
+
+Console.WriteLine(Math.Sqrt(num1));
+
+Console.WriteLine(Math.Pow(num2, 3));
+
+Console.WriteLine(Math.Round(25.61534, 2));
+
+Console.WriteLine(Math.PI * 2);
+*/
+
+Console.WriteLine("Entre com o ano: ");
+int ano = int.Parse(Console.ReadLine());
+
+if(ano %4 == 0)
+{
+    Console.WriteLine($"É um ano bissexto1 {ano %4}");
+}
+else
+{
+    Console.WriteLine($"Esse ano não é bissexto {ano % 4}");
+}
